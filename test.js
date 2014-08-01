c9fc76bf85e00f4790a213fd1e8f5ab7837a2fd5
cvette <vette.christian@gmail.com>
2014-08-01 17:00:54 +0200

    activation rewrite

--- test.js
+++ test.js
@@ -2,26 +2,32 @@ var FIS = require('./lib/fis');
 var Types = require('./lib/function_types');
 var ActivationMethod = require('./lib/activation_methods');
 var AccumulationMethod = require('./lib/accumulation_methods');
-var AndMethod = require('./lib/activation_methods');
-var OrMethod = require('./lib/activation_methods');
+var AndMethod = require('./lib/and_methods');
+var OrMethod = require('./lib/or_methods');
 
-var fis = new FIS(ActivationMethod.Min, AccumulationMethod.Max, AndMethod.Min, OrMethod.Min);
-var test1 = fis.addInput('test1', [0, 10]);
-var test2 = fis.addOutput('test2', [0, 10]);
+var fis = new FIS(ActivationMethod.Min, AccumulationMethod.Max, AndMethod.Min, OrMethod.Max);
+var service = fis.addInput('service', [0, 10]);
+var food = fis.addInput('food', [0, 10]);
+var tip = fis.addOutput('tip', [0, 30]);
 
-test1.addTerm('good', Types.Gaussian, [2, 5]);
-test1.addTerm('ok',   Types.Triangular, [3, 6, 8]);
-test1.addTerm('bad',  Types.GeneralizedBell, [2, 4, 6]);
+service.addTerm('poor', Types.Gaussian, [1.5, 0]);
+service.addTerm('good',   Types.Gaussian, [1.5, 5]);
+service.addTerm('excellent',  Types.Gaussian, [1.5, 10]);
 
-test2.addTerm('good', Types.Gaussian, [2, 5]);
-test2.addTerm('ok',   Types.Triangular, [3, 6, 8]);
-test2.addTerm('bad',  Types.GeneralizedBell, [2, 4, 6]);
+food.addTerm('rancid', Types.Trapezoidal, [-2, 0, 1, 3]);
+food.addTerm('delicious',  Types.Trapezoidal, [7, 9, 10, 12]);
 
-rule1 = fis.createRuleBuilder();
-rule1.if('test1').is('good').then('test2', 'bad').weight(1);
-rule1 = rule1.getRule();
+tip.addTerm('cheap', Types.Triangular, [0, 5, 10]);
+tip.addTerm('average',   Types.Triangular, [10, 15, 20]);
+tip.addTerm('generous',  Types.Triangular, [20, 25, 30]);
 
+fis.createRuleBuilder().if('service').is('poor').or('food').is('rancid').then('tip', 'cheap').create();
+fis.createRuleBuilder().if('service').is('good').then('tip', 'average').create();
+fis.createRuleBuilder().if('service').is('excellent').or('food').is('delicious').then('tip', 'generous').create();
 
-var res = fis.evaluate(3);
+service.setValue(3);
+food.setValue(7);
+
+var res = fis.evaluate();
 
 console.log(res);
